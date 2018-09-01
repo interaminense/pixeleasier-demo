36339137185e9573c412d15c3bfa9d0a6c9b59eb
Adriano <adriano.interaminense@gmail.com>
2018-09-01 19:16:51 -0300

    Add drawnings

---
+++ src/drawings/finn.js
@@ -0,0 +1,28 @@
+const _ = 'transparent';
+const A = '#0F3239';
+const B = '#FFFFFF';
+const C = '#71BE4E';
+const D = '#F6DBD9';
+const E = '#49C9F3';
+const F = '#426AB0';
+
+const draw = [
+	[_, _, A, A, _, _, _, _, _, _, A, A, _, _, ],
+	[_, A, B, B, A, A, A, A, A, A, B, B, A, _, ],
+	[_, A, B, B, B, B, B, B, B, B, B, B, A, _, ],
+	[_, A, B, B, D, D, D, D, D, D, B, B, A, _, ],
+	[_, A, B, D, D, D, D, D, D, D, D, B, A, _, ],
+	[_, A, B, D, A, D, D, D, D, A, D, B, A, _, ],
+	[_, A, B, D, D, D, A, A, D, D, D, B, A, _, ],
+	[_, A, B, B, D, D, D, D, D, D, B, B, A, _, ],
+	[A, C, C, B, B, B, B, B, B, B, B, C, C, A, ],
+	[A, D, C, E, E, E, E, E, E, E, E, C, D, A, ],
+	[A, D, C, E, E, E, E, E, E, E, E, C, D, A, ],
+	[A, D, E, E, E, E, E, E, E, E, E, E, D, A, ],
+	[_, A, E, E, E, E, E, E, E, E, E, E, A, _, ],
+	[_, A, F, F, F, F, F, F, F, F, F, F, A, _, ],
+	[_, A, F, F, F, A, A, A, A, F, F, F, A, _, ],
+	[_, _, A, A, A, _, _, _, _, A, A, A, _, _, ]
+]
+
+export default draw;
---
+++ src/drawings/index.js
@@ -0,0 +1,7 @@
+import finnAndJakeDraw from './finn-and-jake';
+import steveJobsDraw from './steve-jobs';
+import batman from './batman';
+import robotCop from './robot-cop';
+import finn from './finn';
+
+export {finnAndJakeDraw, steveJobsDraw, batman, robotCop, finn};
