77b4cdd96b61193ac85734ca8a8eecaa29bb21be
Adriano <adriano.interaminense@gmail.com>
2018-09-01 19:30:11 -0300

    Fix draw imports

--- src/drawings/index.js
+++ src/drawings/index.js
@@ -1,7 +1,7 @@
 import finnAndJakeDraw from './finn-and-jake';
 import steveJobsDraw from './steve-jobs';
-import batman from './batman';
-import robotCop from './robot-cop';
-import finn from './finn';
+import batmanDraw from './batman';
+import robotCopDraw from './robot-cop';
+import finnDraw from './finn';
 
-export {finnAndJakeDraw, steveJobsDraw, batman, robotCop, finn};
+export {finnAndJakeDraw, steveJobsDraw, batmanDraw, robotCopDraw, finnDraw};
