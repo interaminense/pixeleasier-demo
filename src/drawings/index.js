fc8f6ab435460861becbac1a053d3de22804274d
Adriano <adriano.interaminense@gmail.com>
2018-09-06 00:34:41 -0300

    Add Darth Vader draw

--- src/drawings/index.js
+++ src/drawings/index.js
@@ -3,5 +3,6 @@ import steveJobsDraw from './steve-jobs';
 import batmanDraw from './batman';
 import robotCopDraw from './robot-cop';
 import finnDraw from './finn';
+import vaderDraw from './vader';
 
-export {finnAndJakeDraw, steveJobsDraw, batmanDraw, robotCopDraw, finnDraw};
+export {finnAndJakeDraw, steveJobsDraw, batmanDraw, robotCopDraw, finnDraw, vaderDraw};
---
+++ src/drawings/vader.js
@@ -0,0 +1,42 @@
+
+const _ = "transparent";
+const A = "#050331";
+const B = "#F3FFFE";
+const C = "#726786";
+const D = "#43305F";
+
+const draw = [
+	[_,_,_,_,_,_,A,A,A,A,A,_,_,_,_,_,_,_,_,_],
+	[_,_,_,_,_,A,B,B,B,B,C,A,_,_,_,_,_,_,_,_],
+	[_,_,_,_,A,B,B,C,C,C,D,C,A,_,_,_,_,_,_,_],
+	[_,_,_,_,A,B,C,C,D,D,D,C,A,_,_,_,_,_,_,_],
+	[_,_,_,_,A,B,C,D,D,D,D,C,A,_,_,_,_,_,_,_],
+	[_,_,_,_,A,C,C,C,C,C,D,C,A,A,_,_,_,_,_,_],
+	[_,_,_,A,C,B,D,A,B,A,D,D,C,A,_,_,_,_,_,_],
+	[_,_,_,A,D,D,B,A,A,A,A,D,C,A,_,_,_,_,_,_],
+	[_,_,_,A,D,C,B,C,D,A,A,D,C,C,A,_,_,_,_,_],
+	[_,_,_,A,B,C,B,C,B,D,A,D,D,C,A,_,_,_,_,_],
+	[_,_,_,_,A,A,A,A,A,A,A,D,D,C,A,_,_,_,_,_],
+	[_,_,_,_,A,A,A,A,A,A,A,A,A,A,A,_,_,_,_,_],
+	[_,_,_,A,A,C,D,C,D,C,D,C,D,D,A,A,_,_,_,_],
+	[_,_,A,A,D,B,D,B,D,B,D,C,C,D,D,A,_,_,_,_],
+	[_,_,A,C,C,C,D,C,D,C,D,D,C,A,D,A,_,_,_,_],
+	[_,_,A,C,C,A,A,A,A,A,A,D,C,C,A,D,A,_,_,_],
+	[_,A,A,C,D,A,B,C,B,C,A,A,D,C,A,D,A,_,_,_],
+	[_,A,A,C,D,A,B,C,A,B,A,A,D,C,A,D,A,_,_,_],
+	[_,A,C,C,D,A,B,A,A,B,A,A,D,C,C,A,D,A,_,_],
+	[D,A,C,C,C,C,B,B,B,B,A,C,C,C,C,A,D,A,_,_],
+	[A,A,C,C,A,C,A,A,A,A,A,C,A,C,C,A,D,A,A,_],
+	[A,A,C,C,A,A,D,C,C,C,D,A,A,C,C,A,A,D,A,_],
+	[A,A,A,A,A,D,C,A,A,A,C,D,A,A,A,A,A,D,A,_],
+	[_,A,A,A,A,D,C,A,A,A,C,D,A,A,A,A,A,D,A,_],
+	[_,A,A,A,A,D,C,A,A,A,C,D,A,A,A,A,A,D,A,A],
+	[_,_,A,A,A,C,C,A,A,A,C,C,A,A,A,A,A,D,A,A],
+	[_,_,_,_,A,C,C,A,_,A,C,C,A,_,_,_,_,_,_,_],
+	[_,_,_,_,A,D,C,A,_,A,C,D,A,_,_,_,_,_,_,_],
+	[_,_,_,A,A,D,C,A,_,A,C,D,A,_,_,_,_,_,_,_],
+	[_,_,_,A,C,C,C,A,_,A,C,C,C,A,_,_,_,_,_,_],
+	[_,_,_,A,A,A,A,A,D,A,A,A,A,A,D,D,D,C,C,_]
+];
+
+export default draw;
