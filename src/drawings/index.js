e737ee676869cc0a8b1810c189bff1c93a9922f6
Adriano <adriano.interaminense@gmail.com>
2018-09-10 00:42:44 -0300

    Add new drawings in index

--- src/drawings/index.js
+++ src/drawings/index.js
@@ -4,5 +4,26 @@ import batmanDraw from './batman';
 import robotCopDraw from './robot-cop';
 import finnDraw from './finn';
 import vaderDraw from './vader';
+import hellboyDraw from './hellboy';
+import deloreanDraw from './delorean';
+import capitainAmericaDraw from './captain-america';
+import chewieDraw from './chewie';
+import guardianDraw from './guardian';
+import ninjasDraw from './ninjas';
+import starWarsDraw from './star-wars';
 
-export {finnAndJakeDraw, steveJobsDraw, batmanDraw, robotCopDraw, finnDraw, vaderDraw};
+export {
+	finnAndJakeDraw,
+	steveJobsDraw,
+	batmanDraw,
+	robotCopDraw,
+	finnDraw,
+	vaderDraw,
+	hellboyDraw,
+	deloreanDraw,
+	capitainAmericaDraw,
+	chewieDraw,
+	guardianDraw,
+	ninjasDraw,
+	starWarsDraw
+};
